d930d6b5b1d6627bd6a18bc72ee8e0c8c75e55a3
Aamer Paul <aamerpaul@gmail.com>
2023-12-24 12:10:03 +0530

    update - setup typedefs, resolvers

---
+++ src/schema.ts
@@ -0,0 +1,33 @@
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+export const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Author" type defines the queryable fields for every author in our data source.
+  type Author {
+    id: ID!
+    name: String!
+  }
+  type Review {
+    id: ID!
+    rating: Int!
+    review: String
+  }
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    author: Author!
+    reviews: [Review!]
+    created: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "authors", "posts" query returns an array of zero or more authors, posts (defined above).
+  type Query {
+    authors: [Author]
+    posts: [Post]
+  }
+`;
